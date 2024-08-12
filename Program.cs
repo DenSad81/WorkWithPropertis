3bd4e3701cc02b09b2a1cac86f66acab5277f60a
DenSad81 <tagil81@mail.ru>
2024-08-12 15:49:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp39_WorkWithCPropertis_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero mario = new Hero('$', 1, 2);
+            Renderer renderer = new Renderer();
+
+            renderer.DrowHeroInPosition(mario);
+        }
+    }
+
+    class Hero
+    {
+        public char SignHero { get; }
+        public int PositionX { get; }
+        public int PositionY { get; }
+
+        public Hero(char sign, int positionX = 0, int positionY = 0)
+        {
+            SignHero = sign;
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+    }
+
+    class Renderer
+    {
+        public void DrowHeroInPosition(Hero hero)
+        {
+            Console.SetCursorPosition(hero.PositionX, hero.PositionY);
+            Console.Write(hero.SignHero);
+        }
+    }
+}
